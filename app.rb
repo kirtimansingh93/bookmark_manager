33b8caee18367e6382191f4233d92bfcca283e1b
Kirtiman Singh <kirt93@hotmail.com>
2018-08-02 17:04:40 +0100

    user story 2 - add bookmarks

--- app.rb
+++ app.rb
@@ -20,9 +20,14 @@ class BookmarkManager < Sinatra::Base
     erb :index
   end
 
-  # get '/bookmarks/new' do
-  #   erb(:"bookmarks/new")
-  # end
-
+  get '/bookmarks/new' do
+    erb(:"bookmarks/new")
+  end
+  post '/bookmarks' do
+    url = params['url']
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+    redirect '/bookmarks'
+  end
   run! if app_file == $0
 end
--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -1,9 +1,9 @@
-# feature 'So that I can quickly access a new site' do
-#   scenario 'I would like to add a new bookmark' do
-#     visit('/bookmarks/new')
-#     fill_in('url', with: 'http://testbookmark.com')
-#     click_button('Submit')
-#
-#     expect(page).to have_content 'http://testbookmark.com'
-#   end
-# end
+feature 'So that I can quickly access a new site' do
+  scenario 'I would like to add a new bookmark' do
+    visit('/bookmarks/new')
+    fill_in('url', with: 'http://testbookmark.com')
+    click_button('Submit')
+
+    expect(page).to have_content 'http://testbookmark.com'
+  end
+end
